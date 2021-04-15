593e9fcf105697eef8294c0fbb4709e966901e34
Sebastien Cayetanot <sebastien.cayetanot@gmail.com>
2021-04-15 03:18:43 -0700

    Add crash action

--- app/app/src/main/java/com/example/embracecrash/MainActivity.kt
+++ app/app/src/main/java/com/example/embracecrash/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.embracecrash
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,9 +15,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        lateinit var crashAction: String
+
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val snackbar = Snackbar
+                    .make(view, "Message is deleted", Snackbar.LENGTH_LONG)
+                    .setAction("UNDO") {
+                        crashAction.length
+                        val snackbar1 = Snackbar.make(view, crashAction.length.toString(), Snackbar.LENGTH_SHORT)
+                        snackbar1.show()
+                    }
+            snackbar.show()
         }
     }
 
